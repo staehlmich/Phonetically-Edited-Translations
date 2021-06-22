32e8611d7fae1857b0e2427716a4488c8bbe0587
Michael Staehli <staehli@rattle.ifi.uzh.ch>
2021-06-22 22:30:16 +0200

    22.06: Added weighted levenshtein distance. Fixing errors with compounds and keys.

--- homophone_analysis/homophone_analysis.py
+++ homophone_analysis/homophone_analysis.py
@@ -3,6 +3,7 @@
 #Author: Michael Staehli
 
 import argparse
+import itertools
 import typing
 from typing import Iterator
 from itertools import chain
@@ -13,6 +14,7 @@ import phoneme_dictionary_extract as pde
 import csv
 import pandas as pd
 from nltk import ngrams
+from nltk.metrics import aline
 import json
 import timeit
 
@@ -166,6 +168,122 @@ def minimum_edit_distance(source, target):
                 )
     return d[n][m]
 
+def iterative_levenshtein(s, t, costs=(3, 1, 3)):
+    """
+        iterative_levenshtein(s, t) -> ldist
+        ldist is the Levenshtein distance between the strings
+        s and t.
+        For all i and j, dist[i,j] will contain the Levenshtein
+        distance between the first i characters of s and the
+        first j characters of t
+
+        costs: a tuple or a list with three integers (d, i, s)
+               where d defines the costs for a deletion
+                     i defines the costs for an insertion and
+                     s defines the costs for a substitution
+    """
+    s = s[1:-1].split("><")
+    t = t[1:-1].split("><")
+    rows = len(s) + 1
+    cols = len(t) + 1
+    deletes, inserts, substitutes = costs
+    dist = [[0 for x in range(cols)] for x in range(rows)]
+
+    # source prefixes can be transformed into empty strings
+    # by deletions:
+    for row in range(1, rows):
+        dist[row][0] = row * deletes
+
+    # target prefixes can be created from an empty source string
+    # by inserting the characters
+    for col in range(1, cols):
+        dist[0][col] = col * inserts
+
+    for col in range(1, cols):
+        for row in range(1, rows):
+            if s[row - 1] == t[col - 1]:
+                cost = 0
+            else:
+                cost = substitutes
+            dist[row][col] = min(dist[row - 1][col] + deletes,
+                                 dist[row][col - 1] + inserts,
+                                 sub_cost(s[row - 1],t[
+                                     col - 1]) + cost)  # substitution
+
+    return dist[row][col]
+
+def mod_delta(p, q):
+    """
+    Modified delta function from nltks.metrics.aline. Included the feature
+    "high" in vowels, because there was no difference between some vowels.
+    @param p:
+    @param q:
+    @return:
+    """
+    #Add phonemes from IPA, that are missing in aline.
+    aline.feature_matrix["ɑ"] = {'place': 'vowel', 'manner': 'vowel2',
+                                 'syllabic': 'plus', 'voice': 'plus',
+                                 'nasal': 'minus', 'retroflex': 'minus',
+                                 'lateral': 'minus', 'high': 'low',
+                                 'back': 'back', 'round': 'minus',
+                                 'long': 'minus', 'aspirated': 'minus'}
+    aline.feature_matrix["ʌ"] = {'place': 'vowel', 'manner': 'vowel2',
+                                 'syllabic': 'plus', 'voice': 'plus',
+                                 'nasal': 'minus', 'retroflex': 'minus',
+                                 'lateral': 'minus', 'high': 'mid',
+                                 'back': 'back', 'round': 'minus',
+                                 'long': 'minus', 'aspirated': 'minus'}
+    aline.feature_matrix["ɝ"] = {'place': 'vowel', 'manner': 'vowel2',
+                                 'syllabic': 'plus', 'voice': 'plus',
+                                 'nasal': 'minus', 'retroflex': 'plus',
+                                 'lateral': 'minus', 'high': 'mid',
+                                 'back': 'central', 'round': 'minus',
+                                 'long': 'minus', 'aspirated': 'minus'}
+    aline.feature_matrix["ɪ"] = {'place': 'vowel', 'manner': 'vowel2',
+                                 'syllabic': 'plus', 'voice': 'plus',
+                                 'nasal': 'minus', 'retroflex': 'minus',
+                                 'lateral': 'minus', 'high': 'low-mid',
+                                 'back': 'front', 'round': 'minus',
+                                 'long': 'minus', 'aspirated': 'minus'}
+    aline.feature_matrix["ʊ"] = {'place': 'vowel', 'manner': 'vowel2',
+                                 'syllabic': 'plus', 'voice': 'plus',
+                                 'nasal': 'minus', 'retroflex': 'minus',
+                                 'lateral': 'minus', 'high': 'low-mid',
+                                 'back': 'back', 'round': 'minus',
+                                 'long': 'minus', 'aspirated': 'minus'}
+
+    #Add feature "high" to diffentiate some vowels.
+    aline.R_v = [
+        "back",
+        "lateral",
+        "long",
+        "manner",
+        "nasal",
+        "place",
+        "retroflex",
+        "round",
+        "syllabic",
+        "voice", "high"
+    ]
+
+    features = aline.R(p, q)
+    equal = 0
+    # if p in aline.consonants or q in aline.consonants:
+    #     equal = 8
+    #     for f in features:
+    #         if aline.feature_matrix[p][f] == aline.feature_matrix[q][f]:
+    #             equal -= 1
+    # else:
+    #     equal = 11
+    #     for f in features:
+    #         if aline.feature_matrix[p][f] == aline.feature_matrix[q][f]:
+    #             equal -= 1
+    total = 0
+    for f in features:
+        total += aline.diff(p, q, f) * aline.salience[f]
+
+    return total
+
 def find_phon_levdist(search_string,phon_string, dist:int):
     """
     Find phonetically close tokens
@@ -188,28 +306,75 @@ def find_phon_levdist(search_string,phon_string, dist:int):
             if stress in phon_string:
                 return phon_string
 
+#TODO: can pass function as parameter!
+def find_phon_levdist2(search_string,phon_string, dist:int):
+    """
+    Find phonetically close tokens
+    @param search_string:
+    @param phon_string:
+    @param dist:
+    @return:
+    """
+    #Solution by: https://stackoverflow.com/a/19859340
+    # def num_there(s):
+    #     return any(i.isdigit() for i in s)
+
+    # def stress_in_target(s, stresses):
+    #     for stress in stresses:
+
+    stress = [phon+">" for phon in search_string.split(">") if "1" in phon][0]
+    if phon_string not in search_string:
+        lev_dist = iterative_levenshtein(search_string, phon_string)
+        if lev_dist <= dist:
+            if stress in phon_string:
+                return phon_string
 
 # 2. Levenshtein: Find phonetically close tokens. Can't be substrings.
 # Different weights: stresses, close location!
 # Search for min dist.
 
-# 3. Levenshtein: Find phonetically close tokens in ngram.
-# Different weights: stresses, close location!
+def arpabet_to_ipa(phoneme: str) -> str:
+    """
+    Helper function that looks up an arpabet-symbol
+    and returns IPA-symbol.
+    @param phoneme: phoneme as arpabet-symbol.
+    @return: phoneme as IPA-symbol
+    """
+    codes_lookup = {}
+    with open('arpabet_to_ipa.csv', mode='r') as infile:
+        reader = csv.reader(infile)
+        for line in reader:
+            line = line[0].split()
+            codes_lookup[line[0]] = line[1]
+    return codes_lookup[phoneme]
 
-# ModifiedLev --> class super(Levenshtein))
-def phon_loc():
-    plosives = ["P", "B", "T", "D", "K", "G"]
-    nasals = ["M", "N", "NG"]
-    fricatives = ["F", "V", "TH", "DH", "S", "Z", "SH", "H"]
-    approximant = ["R", "Y"]
-    lateral = ["L"]
-    double_cons = ["JH"]
-    close = []
-    close_mid = []
-    open_mid = []
-    open = ["AA"]
-    near_open = []
-    diphtongues = []
+def sub_cost(p:str, q:str) -> int:
+    """
+    Function that transforms phoneme similarity into costs for levenshtein
+    function.
+    @param p: phoneme as arpabet-symbol
+    @param q: phoneme as arpabet-symbol
+    @return: costs
+    """
+    #Transform arpabet to IPA to look up similarity with nltk.metrics.aline.
+    p = p
+    q = q
+    if len(p) ==1:
+        p = arpabet_to_ipa(p)
+    if len(q) == 1:
+        q = arpabet_to_ipa(q)
+    sub_cost = 0
+    delta = mod_delta(p, q)
+    if delta == 0:
+        return sub_cost
+    elif delta <= 10:
+        sub_cost = 1
+    elif delta > 100:
+        sub_cost = 3
+    else:
+        sub_cost = 2
+    return int(sub_cost)
+
 def main():
 
     # Idea 19.4 --> Train BPE on dictionary and generate learned mappings?
@@ -265,13 +430,61 @@ def main():
     # ngram = ('<W><IY1>', '<T><EY1><K><IH0><NG>>')
     # find_phon_substring(search_string, ngram)
 
+    # TODO: Fix compunds with hypen (split before training MFA).
+    # counter3 = 0
+    # for value in homophones_en["<UNK>"]:
+    #     if "-" in value:
+    #         value = value.split("-")
+    #         for token in value:
+    #             if token not in vocab_phon:
+    #                 print(token)
+
+    #I should work with similar phrases! Not ngrams!
     ex = vocab_phon["play"]
+    #Edit distance with "normal" levenshtein.
+    counter1 = 0
     for key in homophones_en:
-        if find_phon_levdist(ex, key, 1):
-            print(key, homophones_en[key])
+        if key != "<UNK>":
+            if find_phon_levdist(ex, key, 1):
+                print(key, homophones_en[key])
+                counter1 += 1
     print("\n")
-    print(vocab_phon["place"])
-    print(minimum_edit_distance(ex, vocab_phon["place"]))
+    # print(vocab_phon["place"])
+    # print(minimum_edit_distance(ex, vocab_phon["place"]))
+    # print(iterative_levenshtein(ex, vocab_phon["place"]))
+    print("\n")
+
+    #Edit distance with "weighted" levenshtein
+    counter2 = 0
+    for key in homophones_en:
+        if key != "<UNK>":
+            if find_phon_levdist2(ex, key, 1):
+                print(key, homophones_en[key])
+                counter2 +=1
+    print("\n")
+    print("Similar strings f1:", counter1, "Similar strings f2:", counter2)
+
+    ### Tests for phoneme similariy. ###
+    # for p, q in itertools.combinations(aline.consonants, 2):
+    #     p = p.rstrip()
+    #     q = q.rstrip()
+    #
+    #     print(p, q, mod_delta(p, q))
+        # if mod_delta(p, q) == 0:
+            # print(p, aline.feature_matrix[p])
+            # print(q, aline.feature_matrix[q])
+            # print("\n")
+
+    print("\n")
+
+    #
+    # for key in codes_lookup:
+    #     kondrak_key = codes_lookup[key]
+    #     if len(kondrak_key) == 1:
+    #         print("f", kondrak_key, mod_delta("f", kondrak_key))
+    # print(sub_cost("f", "f"))
+    ### Tests for phoneme similariy. ###
+
 
     #TODO: print out grapheme representations of everything --> lookup with original file.
 
